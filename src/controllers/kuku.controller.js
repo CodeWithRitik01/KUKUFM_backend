45ab965d93c84a3a8df2a4ab946ea948fc961720
CodeWithRitik01 <141724500+CodeWithRitik01@users.noreply.github.com>
2024-07-31 12:14:16 +0530

    Update kuku.controller.js

--- src/controllers/kuku.controller.js
+++ src/controllers/kuku.controller.js
@@ -50,9 +50,7 @@ const getTopTen = async(req, res) =>{
 const getVipShows = async(req, res) =>{
     try {
         const response = await axios.get("https://d31ntp24xvh0tq.cloudfront.net/api/v2.1/home/all/?preferred_langs=hindi&page=1&lang=english")
-        const data = response.data.items[4].shows
-
-        console.log(data)
+        const data = response.data.items[3].mixed_content_items
         return res.status(201).json({
             success:true,
             data
